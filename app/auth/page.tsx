81339b87b61a1b9d4646acb98274696f7cc4de03
rifaifirdaus <rifaimuhammad2004@gmail.com>
2026-01-06 05:30:24 +0700

    chore: backend only - removed frontend components, hooks, pages

--- app/auth/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthPage() {
-    return (
-        <div className="flex justify-center items-center min-h-screen">
-            <p className="text-neutral-500">Auth Page</p>
-        </div>
-    );
-}
